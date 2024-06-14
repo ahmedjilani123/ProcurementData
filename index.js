c84c57543dbad75fa94f3613d845d3b634e339e0
ahmedjilani123 <ahmedjilani0011@gmail.com>
2024-06-14 17:56:03 +0530

    ad

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors({
 const port=process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.get("/vendor",async(req,res)=>{
+app.get("/",async(req,res)=>{
 try{
     const data = await mongooseModel.find();
     res.json({"message":"welcome"});
